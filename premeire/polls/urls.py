c1113932cb27dc59a10bea5cb4a911c71a000b2e
Saúl Yaguana <saulyaguanacruz@gmail.com>
2023-02-26 21:23:45 -0500

    Clase 19: Creación de vistas para la app

--- premeire/polls/urls.py
+++ premeire/polls/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    # example: /polls/
+    path("", views.index, name="index"),
+    # example: /polls/5
+    path("<int:question_id>/", views.detail, name="The Detail"),
+    # example: /polls/5/results
+    path("<int:question_identifier>/results/", views.results, name="The Results"),
+    # example: /polls/5/vote
+    path("<int:question_id>/vote/", views.vote, name="voting")
 ]
--- premeire/polls/views.py
+++ premeire/polls/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Hello World with Django")
+    return HttpResponse("Estas en la página principal de Premiere")
+
+def detail(request, question_id):
+    return HttpResponse(f'Estas viendo la pregunta {question_id}')
+
+
+def results(request, question_identifier):
+    return HttpResponse(f'Estas viendo los resultados de la pregunta {question_identifier}')
+
+def vote(request, question_id):
+    return HttpResponse(f'Estas votando la pregunta {question_id}')
