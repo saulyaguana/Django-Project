3cd4d8fb68d6aea37bbc21ddd99e4c90dd5bfd2f
Saúl Yaguana <saulyaguanacruz@gmail.com>
2023-03-02 22:05:41 -0500

    Clase 22: Elevando el error 404

--- premeire/polls/views.py
+++ premeire/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -9,7 +9,10 @@ def index(request):
     })
 
 def detail(request, question_id):
-    return HttpResponse(f'Estas viendo la pregunta {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 
 def results(request, question_identifier):
