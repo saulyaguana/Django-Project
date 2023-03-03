f7303df8b1e1cd5ae2485a9a099aa9fd5b30c93f
Saúl Yaguana <saulyaguanacruz@gmail.com>
2023-03-02 22:31:47 -0500

    Clase 23: etiqueta url para evitar hard coding

--- premeire/polls/urls.py
+++ premeire/polls/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
     # example: /polls/
     path("", views.index, name="index"),
     # example: /polls/5
-    path("<int:question_id>/", views.detail, name="The Detail"),
+    path("<int:question_id>/the_best_details/", views.detail, name="The Detail"),
     # example: /polls/5/results
     path("<int:question_identifier>/results/", views.results, name="The Results"),
     # example: /polls/5/vote
