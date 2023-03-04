577a96887fc70d042cf916a36ebaeaaf09a05054
Saúl Yaguana <saulyaguanacruz@gmail.com>
2023-03-04 14:10:54 -0500

    Clase 26: Creación de la vista vote

--- premeire/polls/views.py
+++ premeire/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.all()
@@ -19,4 +20,15 @@ def results(request, question_identifier):
     return HttpResponse(f'Estas viendo los resultados de la pregunta {question_identifier}')
 
 def vote(request, question_id):
-    return HttpResponse(f'Estas votando la pregunta {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "No elegiste una respuesta."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:The Results", args=(question.id,)))
